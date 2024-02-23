c51d435782534ba05313602383a3aa1a6117b595
Airan <alexisjuhell77100@gmail.com>
2024-02-23 10:29:02 +0100

    Schéma

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_22_154831) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_23_092511) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_22_154831) do
     t.float "price"
     t.text "description"
     t.boolean "veggie"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "fries", force: :cascade do |t|
+    t.text "image"
+    t.string "name"
+    t.float "price"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
